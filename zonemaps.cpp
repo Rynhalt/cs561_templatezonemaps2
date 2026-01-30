f396b47e72d7360eac32926b586d1b8958152474
Marcus Izumi <slime123@scc-wl4.scc.bu.edu>
2026-01-30 14:14:57 -0500

    basic initialization

--- zonemaps.cpp
+++ zonemaps.cpp
@@ -4,7 +4,15 @@ template<typename T>
 zonemap<T>::zonemap(std::vector<T> _elements, uint _num_elements_per_zone)
 {
     // constructor 
+
     // Your code starts here ...
+
+    // storing inputs
+    elements = std::move(_elements);
+    // Instead of copying elements, transferring _elements to an xvalue to reuse resources!
+    num_elements_per_zone = _num_elements_per_zone;
+    // move() does not provide benefit for a uint
+
 }
 
 template<typename T>
